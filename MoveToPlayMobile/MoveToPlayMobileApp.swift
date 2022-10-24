7a68641dd7df0c56aa17cd19567b4944d351a650
Kyrgyzbaev Danil <kyrgyzbaev-danil>
2022-10-24 21:56:25 +0600

    Initial Commit

---
+++ MoveToPlayMobile/MoveToPlayMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveToPlayMobileApp.swift
+//  MoveToPlayMobile
+//
+//  Created by Kyrgyzbaev Danil on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoveToPlayMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
